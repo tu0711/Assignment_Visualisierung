17db4fcebdb1c9e1e8d79bcb316eb699e12cea50
tu0711 <dipseltuce@gmail.com>
2025-11-04 12:09:41 +0100

    Skalierung und Padding für Axen hinzugefügt; radius fehlt immernoch

--- visualization.js
+++ visualization.js
@@ -1,27 +1,47 @@
 
 d3.csv("gapminder.csv").then(function(dataset) {
-  const svgWidth = 10000;
-  const svgHeight = 10000;
-let param = "Average Daily Income";
-    
+
+  
+
+  const svgWidth = 1000;
+  const svgHeight = 1000;
+  let param = "Average Daily Income";
+  let param2 = "Babies per Woman";
+
+
+
+  let minVal = d3.min(dataset, d => d[param2]); 
+  let maxVal = d3.max(dataset, d => d[param2]);                               // Skalierung von Y Achse 
+  let scaleY = d3.scaleLinear().domain([minVal,maxVal]).range([svgHeight, 0]);     // svgHeight und 0 umgekehrt damit 0/0 unten links ist 
+
+    let minValX = d3.min(dataset, d => d[param]); 
+  let maxValX = d3.max(dataset, d => d[param]);                                 // Skalierung von X Achse 
+  let scaleX = d3.scaleLinear().domain([minValX,maxValX]).range([0, svgWidth]);     
+
+
+  // AXES 
+
+  const margin = {top: 20, right: 30, bottom: 30, left: 100}; 
 
 
 let svg = d3.select("svg")
-  .attr("width", svgWidth)
-  .attr("height", svgHeight)
+  .attr("width", svgWidth + margin.left + margin.right)  
+  .attr("height", svgHeight  + margin.top + margin.bottom)
   .attr("class", "chart");
 
-let chart = svg.selectAll("circle")
+let chart = svg.append("g")                                                       // aus Vorlesung    
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")         // aus Vorlesung     
+.selectAll("circle")
 .data(dataset)
 .enter()
 .append("circle")
 .attr("cx", function(d) {   
-    return d["Average Daily Income"] * 100   ;   // cx aus CSV
-
+    return scaleX(d[param]);    // cx aus CSV / Average Income 
     })
 .attr("cy", function(d) {
-return d["Babies per Woman"] * 100 ; })
-.attr("r", "20")
+
+return scaleY(d[param2]) ; })   // cy aus CSV / Babies per Woman 
+.attr("r", "20" )               // 20 als Platzhalter, Verhältniss zur Population nicht codiert 
 .attr("fill", "red");
 
 
@@ -30,4 +50,5 @@ console.log(reason); // Error! In many browsers, press F12 to see the console d3
     d3.select("body")
      .append("p")
     .text("Could not load data set. See console for more information."); 
+  
 });
